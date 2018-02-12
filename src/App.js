36a31332f86c7ed54cfc9a9259b13a78a851e543
CORP\rrambani <rosys9319@gmail.com>
2018-02-12 15:13:40 -0500

    changes done to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react'
-
 class App extends Component {
-	
+render() {
+return (
+<div className="app">
+	<div className="app-header">
+		<h2>Welcome to React</h2>
+	</div>
+	<p className="app-intro">
+		This is a demo react app.
+	</p>
+</div>
+)
 }
+}
+
+export default App
